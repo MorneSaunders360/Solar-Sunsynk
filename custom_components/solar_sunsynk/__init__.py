b1df4778869fa04fc6ffba5e62973c499e101a11
ITLife360 <109594480+ITLife360@users.noreply.github.com>
2023-04-19 21:35:00 +0200

    Update __init__.py

--- custom_components/solar_sunsynk/__init__.py
+++ custom_components/solar_sunsynk/__init__.py
@@ -1,76 +1,78 @@
+import logging
 import requests
 import json
 from datetime import timedelta
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant
-from homeassistant.helpers.entity import Entity
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
+from .const import DOMAIN
 
-DOMAIN = "solar_sunsynk"
 SCAN_INTERVAL = timedelta(seconds=120)
 
-async def async_setup(hass: HomeAssistant, config: dict):
-    return True
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+_LOGGER = logging.getLogger(__name__)
+
+async def async_setup_entry(hass, entry):
+    """Set up the Solar Sunsynk component."""
     username = entry.data.get('username')
     password = entry.data.get('password')
 
-    async def update_states(now=None):
+    if username is None or password is None:
+        _LOGGER.error("Missing required configuration items %s or %s", 'username', 'password')
+        return False
+
+    async def async_update_data():
         urlAuth = "https://solarsunsynk.houselabs.co.za/api/GetToken"  # Replace with your endpoint
         paramsAuth = {"username": username, "password": password}  # Use the username and password from config
         url = "https://pv.inteless.com/api/v1/plants?page=1&limit=10&name=&status="
 
-        responseAuth = requests.get(urlAuth, params=paramsAuth)
-        if responseAuth.ok:
-            token = responseAuth.text
-            headers = {"Authorization": f"Bearer {token}"}
-
-        response = requests.get(url, headers=headers)
-
-        if not response.ok:
-            hass.states.set("solar_sunsynk.error", "Failed")
-        else:
-            data = json.loads(response.content)["data"]
-            infos = data["infos"]
-            for info in infos:
-                # Define the entity ID as "solar_sunsynk.thumb"
-                entity_idthumb = "solar_sunsynk.thumb"
-                # Set the state for the entity
-                hass.states.async_set(entity_idthumb, info["thumbUrl"])
-                
-                for keyinfo, valueInfo in info.items():
-                    if keyinfo == 'plantPermission' or valueInfo is None:
-                        continue
-                    # Define the entity ID as "solar_sunsynk.<key>"
-                    entity_idinfo = f"solar_sunsynk.{keyinfo}"
-                    # Set the state for the entity
-                    hass.states.async_set(entity_idinfo, valueInfo)
-                        
-                
-                id = info["id"]
-                url = f"https://pv.inteless.com/api/v1/plant/energy/{id}/flow"
+        def fetch_data():
+            responseAuth = requests.get(urlAuth, params=paramsAuth)
+            if responseAuth.ok:
+                token = responseAuth.text
+                headers = {"Authorization": f"Bearer {token}"}
                 response = requests.get(url, headers=headers)
-                response_code = response.status_code
-                result = json.loads(response.content)
+                if response.ok:
+                    data = json.loads(response.content)["data"]
+                    infos = data["infos"]
+                    for info in infos:
+                        # Define the entity ID as "solar_sunsynk.thumb"
+                        entity_idthumb = "solar_sunsynk.thumb"
+                        # Set the state for the entity
+                        hass.states.async_set(entity_idthumb, info["thumbUrl"])
 
-                # Set Home Assistant sensor entities for all data in the result
-                for key, value in result["data"].items():
-                    if value is None:
-                        continue
-                    # Check if the key is numeric
-                    entity_id = f"solar_sunsynk.{key}"
-                    # Set the state for the entity
-                    hass.states.async_set(entity_id, value)
+                        for keyinfo, valueInfo in info.items():
+                            if keyinfo == 'plantPermission' or valueInfo is None:
+                                continue
+                            # Define the entity ID as "solar_sunsynk.<key>"
+                            entity_idinfo = f"solar_sunsynk.{keyinfo}"
+                            # Set the state for the entity
+                            hass.states.async_set(entity_idinfo, valueInfo)
 
-    # Schedule the update_states function to run every 2 minutes
-    hass.helpers.event.async_track_time_interval(update_states, SCAN_INTERVAL)
+                        id = info["id"]
+                        url = f"https://pv.inteless.com/api/v1/plant/energy/{id}/flow"
+                        response = requests.get(url, headers=headers)
+                        response_code = response.status_code
+                        result = json.loads(response.content)
 
-    # Return boolean to indicate that initialization was successful.
-    return True
+                        # Set Home Assistant sensor entities for all data in the result
+                        for key, value in result["data"].items():
+                            if value is None:
+                                continue
+                            # Check if the key is numeric
+                            entity_id = f"solar_sunsynk.{key}"
+                            # Set the state for the entity
+                            hass.states.async_set(entity_id, value)
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
-    # Remove entities that belong to the integration
-    await asyncio.gather(
-        *[hass.config_entries.async_forward_entry_unload(entry, domain) for domain in ["sensor"]]
+        await hass.async_add_executor_job(fetch_data)
+
+    coordinator = DataUpdateCoordinator(
+        hass,
+        _LOGGER,
+        name="solar_sunsynk",
+        update_method=async_update_data,
+        update_interval=SCAN_INTERVAL,
     )
+
+    await coordinator.async_refresh()
+
+    hass.data[DOMAIN] = coordinator
 
     return True
