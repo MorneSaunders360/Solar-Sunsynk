7e9f15b90b06fface18fa0f4b4eade9e6d217fdc
ITLife360 <mornesaunders32@gmail.com>
2023-05-27 21:07:54 +0200

    Update __init__.py

--- custom_components/solar_sunsynk/__init__.py
+++ custom_components/solar_sunsynk/__init__.py
@@ -23,8 +23,8 @@ def setup(hass: core.HomeAssistant, entry: config_entries.ConfigEntry):
     identifiers={(DOMAIN, '1.0.0.0')},  # Assuming 'id' is the unique identifier for the device
     name="API",  # Assuming 'name' is the name of the device
     manufacturer="MorneSaunders360",  # Replace with the manufacturer's name
-    model="Sunsynk",  # Replace with the model of the device
-    sw_version="1.0.0."  # Replace with the software version of the device
+    model="API",  # Replace with the model of the device
+    sw_version="1.0.0.1"  # Replace with the software version of the device
     )
     hass.async_create_task(
         hass.config_entries.async_forward_entry_setup(entry, "sensor")
