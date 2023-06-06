5c63f96bd2accd175c7dde179947a49db4a26764
ITLife360 <mornesaunders32@gmail.com>
2023-06-07 01:07:28 +0200

    Sensors updated
    Added measure to sensor

--- custom_components/solar_sunsynk/sensor.py
+++ custom_components/solar_sunsynk/sensor.py
@@ -8,6 +8,7 @@ import json
 import logging
 import time
 import re
+from datetime import datetime
 # Constants
 DOMAIN = "solar_sunsynk"
 _LOGGER = logging.getLogger(__name__)
@@ -16,7 +17,7 @@ DEVICE_INFO = {
     "name": "Solar Sunsynk",
     "manufacturer": "MorneSaunders360",
     "model": "Sunsynk API",
-    "sw_version": "1.0.8",
+    "sw_version": "1.0.9",
 }
 UPDATE_INTERVAL = 10
 async def async_setup_entry(hass, config_entry, async_add_entities):
@@ -51,13 +52,14 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
 
 class SolarSunSynkSensor(SensorEntity):
     """Representation of a sensor entity for Solar Sunsynk data."""
-    def __init__(self, coordinator, result_key,device,id):
+
+    def __init__(self, coordinator, result_key, device, id):
         """Initialize the sensor."""
         self.coordinator = coordinator
         self.result_key = result_key
-        self.device = device 
-        self.id = id 
-        
+        self.device = device
+        self.id = id
+
     @property
     def device_info(self):
         """Return device information."""
@@ -69,29 +71,49 @@ class SolarSunSynkSensor(SensorEntity):
             "sw_version": self.device.sw_version,
             "via_device": (DOMAIN, self.device.id),
         }
-        
+
     @property
     def unique_id(self):
         """Return a unique ID."""
-        # _LOGGER.debug("entities: %s", f"sunsynk_{self.id}_{self.result_key}")
         return f"sunsynk_{self.result_key}"
-
-
 
     @property
     def name(self):
         """Return the name of the sensor."""
         return f"sunsynk_{self.id}_{self.result_key}"
 
-    def _format_key_to_title(self, key):
-        """Format the key in the format 'totalPower' to 'Total Power'."""
-        s1 = re.sub('(.)([A-Z][a-z]+)', r'\1 \2', key)
-        return re.sub('([a-z0-9])([A-Z])', r'\1 \2', s1).title()
-
     @property
     def state(self):
         """Return the state of the sensor."""
         return self.coordinator.data[self.result_key]
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit this state is expressed in."""
+        if self.result_key in ["pvPower", "battPower", "gridOrMeterPower", "loadOrEpsPower", "genPower", "minPower", "pac", "etoday", "etotal", "emonth", "eyear", "income", "totalPower"]:
+            return "kWh"
+        elif self.result_key == "soc" or self.result_key == "efficiency":
+            return "%"
+        else:
+            return None
+
+    @property
+    def device_class(self):
+        """Return the device class of the sensor."""
+        if self.result_key == "soc":
+            return "battery"
+        elif self.result_key in ["pvPower", "battPower", "gridOrMeterPower", "loadOrEpsPower", "genPower", "minPower", "pac", "etoday", "etotal", "emonth", "eyear", "income", "totalPower"]:
+            return "power"
+        else:
+            return None
+
+    @property
+    def state_class(self):
+        """Return the state class of the sensor."""
+        if self.result_key in ["pvPower", "battPower", "gridOrMeterPower", "loadOrEpsPower", "genPower", "minPower", "soc", "pac", "efficiency", "etoday", "etotal", "emonth", "eyear", "income", "totalPower"]:
+            return "measurement"
+        else:
+            return None
 
     async def async_update(self):
         """Update the sensor."""
@@ -119,8 +141,6 @@ async def get_token(session, USERNAME, PASSWORD, API_URL):
         _LOGGER.error("Failed to get token: %s", e)
         return None, None
 
-
-
 async def fetch_data(session, config_entry):
     USERNAME = config_entry.data["username"]
     PASSWORD = config_entry.data["password"]
@@ -141,28 +161,42 @@ async def fetch_data(session, config_entry):
         infos = data["data"]["infos"]
     
     # Combine all the plant data into a single dictionary
+# Combine all the plant data into a single dictionary
     combined_data = {}
     for info in infos:
         id = info["id"]
-        info = {k: str(v)[:255] for k, v in info.items() if v is not None and k != "plantPermission"}
+        # Format dates
+        if "updateAt" in info:
+            updateAt_object = datetime.strptime(info["updateAt"], "%Y-%m-%dT%H:%M:%SZ")
+            info["updateAt"] = updateAt_object.strftime("%Y/%m/%d %H:%M")
+        if "createAt" in info:
+            createAt_object = datetime.strptime(info["createAt"], "%Y-%m-%dT%H:%M:%S.%f%z")
+            info["createAt"] = createAt_object.strftime("%Y/%m/%d %H:%M")
+
+        info = {k: str(v)[:255] for k, v in info.items() if v is not None and k != "plantPermission" and k != "custCode"}
         combined_data.update(info)
 
         # Fetch additional data for each plant
         # Fetch energy flow data
         async with session.get(f"{API_URL}api/v1/plant/energy/{id}/flow", headers=headers) as resp:
             energy_flow_data = await resp.json()
-            energy_flow_data = {k: str(v)[:255] for k, v in energy_flow_data["data"].items() if v is not None}
+            energy_flow_data = {k: str(v)[:255] for k, v in energy_flow_data["data"].items() if v is not None and k != "custCode"}
             combined_data.update(energy_flow_data)
-
 
         # Fetch realtime data
         async with session.get(f"{API_URL}api/v1/plant/{id}/realtime?id={id}", headers=headers) as resp:
             realtime_data = await resp.json()
+            # Format dates
+            if "updateAt" in realtime_data["data"]:
+                updateAt_object = datetime.strptime(realtime_data["data"]["updateAt"], "%Y-%m-%dT%H:%M:%SZ")
+                realtime_data["data"]["updateAt"] = updateAt_object.strftime("%Y/%m/%d %H:%M")
+
             realtime_data = {
                 k: v["code"] if k == "currency" else str(v)[:255] 
                 for k, v in realtime_data["data"].items() 
-                if v is not None
+                if v is not None and k != "custCode"
             }
             combined_data.update(realtime_data)
             
     return combined_data
+
