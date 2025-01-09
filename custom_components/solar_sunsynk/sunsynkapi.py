9e1e809d5716c9758725ba56c28178e8bf579478
Morne <60499349+MorneSaunders360@users.noreply.github.com>
2025-01-09 13:12:54 +0200

    Update sunsynkapi.py

--- custom_components/solar_sunsynk/sunsynkapi.py
+++ custom_components/solar_sunsynk/sunsynkapi.py
@@ -1,4 +1,5 @@
-
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 from homeassistant.core import HomeAssistant
 from datetime import datetime, timedelta
 from .enums import SunsynkApiNames
